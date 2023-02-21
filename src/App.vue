b2b3e44122364f8fa17e6a943ea8a8c49b25c274
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-02-21 13:36:36 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script >
+</script>
+
+<template>
+    
+</template>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      mobile: '375px',
+      tablet: '768px',
+      desktop: '1440px',
+    },
+    colors:{
+      red: "#F87070",
+      cyan: "#70F3F8",
+      purple: "#D881F8",
+      grey: "#D7E0FF",
+      silver: "#EFF1FA",
+      blue: "#1E213F",
+      "dark-blue": "#161932"
+    },
+    fontFamily: {
+      kumbhs: ["Kumbh Sans"],
+      roboto: ["Roboto Slab"],
+      space: ["Space Mono"],
+    },
+    fontSize: {
+        'body-1': ['13px', {lineHeight: '16px', letterSpacing: "5px"}],
+        'body-2': ['14px', {lineHeight: '18px'}],
+        xs: [
+          '12px',
+          {
+            lineHeight: '14px',
+          },
+        ],
+        base: [
+          '16px',
+          {
+            lineHeight: '19px',
+            letterSpacing: '15px',
+          },
+        ],
+        lg: ['28px', {lineHeight: '34px'}],
+        xl: ['100px', {lineHeight: '120px', letterSpacing: '-5px'}],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
